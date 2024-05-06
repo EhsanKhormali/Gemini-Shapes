fbfa7435906e6c8e54c4441e86c2ede8ab9a4bf9
Ehsan <ehsankhormali@gmail.com>
2024-05-06 17:34:56 +0300

    Add Rectangle and Circle model classes

--- app/src/main/java/com/ehsankhormali/myapplication/models/Circle.kt
+++ app/src/main/java/com/ehsankhormali/myapplication/models/Circle.kt
@@ -1,4 +1,8 @@
 package com.ehsankhormali.myapplication.models
 
+import androidx.compose.ui.geometry.Offset
+
 class Circle {
+    var radius=0
+    var center:Offset=Offset(0f,0f)
 }
---
+++ app/src/main/java/com/ehsankhormali/myapplication/models/Rectangle.kt
@@ -0,0 +1,11 @@
+package com.ehsankhormali.myapplication.models
+
+import androidx.compose.ui.geometry.Offset
+
+class Rectangle {
+    var topLeft:Offset=Offset(0f,0f)
+    var topRight:Offset=Offset(0f,0f)
+    var bottomLeft:Offset=Offset(0f,0f)
+    var bottomRight:Offset=Offset(0f,0f)
+
+}
