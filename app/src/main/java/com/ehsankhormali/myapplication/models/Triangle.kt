5ed14227ba0b1e4e3603fcb296def4d5e272231c
Ehsan <ehsankhormali@gmail.com>
2024-05-06 16:45:04 +0300

    Define triangle shape

--- app/src/main/java/com/ehsankhormali/myapplication/models/Triangle.kt
+++ app/src/main/java/com/ehsankhormali/myapplication/models/Triangle.kt
@@ -1,4 +1,9 @@
 package com.ehsankhormali.myapplication.models
 
+import androidx.compose.ui.geometry.Offset
+
 class Triangle {
+    val point1:Offset=Offset(0f,0f)
+    val point2:Offset=Offset(0f,0f)
+    val point3:Offset=Offset(0f,0f)
 }
