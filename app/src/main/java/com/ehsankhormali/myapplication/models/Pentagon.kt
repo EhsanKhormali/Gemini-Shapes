6f64c2df129cdae81ea84294965b83a8ed7830ba
Ehsan <ehsankhormali@gmail.com>
2024-05-09 23:29:04 +0300

    Add Pentagon points

--- app/src/main/java/com/ehsankhormali/myapplication/models/Pentagon.kt
+++ app/src/main/java/com/ehsankhormali/myapplication/models/Pentagon.kt
@@ -1,4 +1,11 @@
 package com.ehsankhormali.myapplication.models
 
+import androidx.compose.ui.geometry.Offset
+
 class Pentagon {
+    var point1= Offset(0f,0f)
+    var point2= Offset(0f,0f)
+    var point3 = Offset(0f,0f)
+    var point4= Offset(0f,0f)
+    var point5 = Offset(0f,0f)
 }
